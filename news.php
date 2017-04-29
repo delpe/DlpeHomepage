07081a4511aea849070979e5ff845b777ca16013
_dlpe <panglod@o2.pl>
2017-04-29 17:19:50 +0200

    Add files via upload

---
+++ news.php
@@ -0,0 +1,30 @@
+<html>
+<!-- dlpe.me - no mercy since 2014 -->
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<title>DLPE.ME | SINCE 2014</title>
+</head>
+<body>
+<center>
+<img class="logo" src="images/logo.png">
+<br><br>
+<ul>
+  <li><a href="index.html">home</a></li>
+  <li><a class="active" href="#">news</a></li>
+  <li><a href="aboutme.html">about me</a></li>
+  <li><a href="http://community.dlpe.me">community</a></li>
+</ul>
+<br><br>
+<img height="164px" width="164px" src="images/wave.gif">
+<br>
+work in progress &#x2639;
+</center>
+</body>
+<footer>
+	<div class="footer">&copy; 2014 - 2017 dlpe.me</div>
+</footer>
+<!-- dlpe.me - no mercy since 2014 -->
+</html>
